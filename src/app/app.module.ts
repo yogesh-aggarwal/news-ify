b86d6d7f08b554008d7128ce52c779eb92be9b31
Yogesh Aggarwal <developeryogeshgit@gmail.com>
2020-05-26 06:38:33 +0530

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SettingsComponent } from './routes/settings/settings.component';
+import { ArticleCardComponent } from './components/article-card/article-card.component';
+import { ArticleComponent } from './components/article/article.component';
+import { HomeComponent } from './routes/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    SettingsComponent,
+    ArticleCardComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
